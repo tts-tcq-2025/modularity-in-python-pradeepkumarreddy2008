5a0cfbc356c9998728ba29e3b96e692635634cb7
pradeepkumarreddy2008 <pradeepkumarpundla@gmail.com>
2025-10-16 10:38:56 +0530

    Update test_color_function.py

--- test_color_function.py
+++ test_color_function.py
@@ -17,7 +17,6 @@ def test_pair_to_number(major_color, minor_color, expected_pair_number):
     pair_number = get_pair_number_from_color(major_color, minor_color)
     assert (pair_number == expected_pair_number)
 
-
 def run_all_tests():
     """Run all test cases."""
     test_number_to_pair(4, 'White', 'Brown')
